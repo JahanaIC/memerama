7ad77d03dbf55ee4cdbb332cdb4f9e4de6df06aa
Jahana Campos <alpha.ilhuicatzi16@tectijuana.edu.mx>
2019-10-15 10:55:55 -0700

    Tap to switch card visibility

---
+++ src/components/Card.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Dimensions, View, Image, StyleSheet } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const cardWidth = (width - 108) / 4
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+        backgroundColor: 'black',
+    },
+    
+    image:{
+        height: cardWidth,
+        opacity: 0,
+        width: cardWidth,
+    },
+
+    imageShown: {
+        opacity: 1,
+    },
+});
+
+class Card extends React.Component{
+    render() {
+        return (
+              <View style={styles.container}>
+                <Image 
+                source={this.props.source} 
+                style={[
+                    styles.image,
+                    this.props.shown && styles.imageShown,
+                ]} 
+                />
+              </View>
+        );
+    }
+}
+
+export default Card;
---
+++ src/components/Cards.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { StyleSheet, FlatList, TouchableWithoutFeedback, View } from "react-native";
+import Card from "./Card";
+import meme0 from "../../assets/0.jpg";
+import meme1 from "../../assets/1.jpg";
+import meme2 from "../../assets/2.jpg";
+import meme3 from "../../assets/3.jpg";
+import meme4 from "../../assets/4.jpg";
+import meme5 from "../../assets/5.jpg";
+import meme6 from "../../assets/6.jpg";
+import meme7 from "../../assets/7.jpg";
+import meme8 from "../../assets/8.jpg";
+import meme9 from "../../assets/9.jpg";
+
+const memes = [
+  meme0,
+  meme1,
+  meme2,
+  meme3,
+  meme4,
+  meme5,
+  meme6,
+  meme7,
+  meme8,
+  meme9
+];
+
+const sources = [...memes, ...memes];
+
+const styles = StyleSheet.create({
+  cardsContainer: {
+    alignItems: "center",
+    paddingVertical: 10 //Pone el espacio arriba y abajo del cuadrado
+  }
+});
+
+class Cards extends React.Component {
+  state = {
+    shownCardsIndexes: []
+  };
+
+  keyExtractor = (_, index) => String(index);
+
+  renderCard = ({ index, item }) => {
+    const onCardPress = () => {
+      this.setState({
+        shownCardsIndexes: [...this.state.shownCardsIndexes, index],
+      });
+    };
+
+    return (
+      <TouchableWithoutFeedback onPress={onCardPress}>
+        <View>
+          <Card
+            shown={this.state.shownCardsIndexes.includes(index)}
+            source={item}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
+  render() {
+    return (
+      <FlatList
+        contentContainerStyle={styles.cardsContainer}
+        data={sources}
+        extraData={this.state.shownCardsIndexes}
+        keyExtractor={this.keyExtractor}
+        numColumns={4}
+        renderItem={this.renderCard}
+        />
+    );
+  }
+}
+
+export default Cards;
